08563e44432ef6545e8c3362f0d1d6d6030881dd
Ashu Gautam <ashugautam009@gmail.com>
2024-06-12 06:05:02 +0530

    Episode 05

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RestaurantCard from './RestaurantCard'
 import {restaurantList} from '../Utils/mockData';
+
+
 const Body = () => {
-  console.log('data on the body page',restaurantList)
+  console.log('data on the body page',restaurantList);
+
+  //state variable -- Super powerful variable
+  const[listOfResturant,setlistOfResturant]=useState(restaurantList);
+
+  //Normal JS Variable
+  // let listofResturantjs=[
+    
+  // ]
   return (
     <div className='body'>
+        
         <div className='Search'>Search</div>
+        
+        <div className='filter'>
+          <button className='filter-btn' onClick={()=>{
+            filterResturant=listOfResturant.filter((res)=> {
+              return res.data.avgRating>4 
+          })
+            setlistOfResturant(filterResturant);
+            console.log('4 rating resturant are ',filterResturant);
+          }}>Top  rated returant </button>
+        </div>
+
         <div className='res-conatainer'>
             {
-              restaurantList.map((restaurant)=><RestaurantCard key={restaurant.data.id} resData={restaurant}/>)
+              listOfResturant.map((restaurant)=><RestaurantCard key={restaurant.data.id} resData={restaurant}/>)
             }  
         </div>  
     </div>
